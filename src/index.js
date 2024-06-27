8c4de83e8975ae17d701a3dccc57d5aa9f364d5b
Jimmy García <jegarcia@a4sys.com>
2024-06-27 09:55:25 -0600

    fix 06272024

--- src/index.js
+++ src/index.js
@@ -31,6 +31,55 @@ app.get("/configurations/v1/webformssetup", async (req, res) => {
   res.send({ status: "OK", data });
 });
 
+app.get("/configurations/v1/webformssetup/:id", async (req, res) => {
+  const { data, error } = await supabase
+    .from("dicWebFormsComponentes")
+    .select()
+    .eq("id", req.params.id)
+
+  console.log("data", data);
+  console.log("error", error);
+
+  res.send({ status: "OK", data });
+});
+
+app.get("/configurations/v1/webformssetup/category/:id", async (req, res) => {
+  const { data, error } = await supabase
+    .from("dicWebFormsComponentes")
+    .select()
+    .eq("idWebFormComponenteTipo", req.params.id)
+
+  console.log("data", data);
+  console.log("error", error);
+
+  res.send({ status: "OK", data });
+});
+
+app.get("/configurations/v1/webformstypes", async (req, res) => {
+  console.log("/configurations/v1/webformstypes/")
+
+  const { data, error} = await supabase
+    .from("dicWebFormsComponentesTipos")
+    .select()
+
+    console.log("data", data)
+    console.log("error", error)
+
+    res.send({ status: "OK", data})
+})
+
+app.get("/configurations/v1/webformstypes/:id", async(req,res) => {
+  const { data, error } = await supabase
+    .from("dicWebFormsComponentesTipos")
+    .select()
+    .eq("id", req.params.id)
+
+    console.log("data", data)
+    console.log("error", error)
+    
+    res.send({ status: "OK", data})
+})
+
 app.get("/products", async (req, res) => {
   const { data, error } = await supabase.from("products").select();
 
